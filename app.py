319094b29015a1258ed34d8d99f406025e09b811
HopstairCDI <159264522+HopstairCDI@users.noreply.github.com>
2024-02-07 13:31:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def create_conversational_chain(vector_store):
 def main():
     # Initialize session state
     initialize_session_state()
-    st.title("Multi-PDF ChatBot using Mistral-7B-Instruct :books:")
+    st.title("AI Buddy - Your Personal Advisor ")
     # Initialize Streamlit
     st.sidebar.title("Document Processing")
     uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
