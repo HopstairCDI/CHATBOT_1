b63639ba0af41fffeea999f89bea56a5b25cd12d
HopstairCDI <159264522+HopstairCDI@users.noreply.github.com>
2024-02-11 11:09:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_chat import message
 from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import LlamaCpp
+from langchain_community.llms import LlamaCpp
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
